518a9df87d66a53335901023f788171541684779
Mohammad <mh.shabanzadeh1@gmail.com>
2025-11-30 18:13:21 +0330

    resolve src

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import HomeProperties from "./pages/HomeProperties";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
